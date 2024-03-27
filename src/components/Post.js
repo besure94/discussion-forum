bd22aa3f220026d4339f90a63921a73672ff102f
Brian Scherner <brianscherner@gmail.com>
2024-03-27 11:18:59 -0700

    Add props to Post to allow multiple posts to be submitted.

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,15 +1,25 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Post() {
-  const post = "Humpback whale off of the Alaskan coast";
-  const post2 = "Deftones playing live on Conan O'Brien in 1998";
+function Post(props) {
   return (
     <React.Fragment>
-      <h3>{post}</h3>
-      <h3>{post2}</h3>
+      <h3>{props.title}</h3>
+      <h4>Submitted by {props.author} on {props.date} in {props.forum}</h4>
+      <h4>Upvotes: {props.upvotes}</h4>
+      <h4>Downvotes: {props.downvotes}</h4>
       <hr/>
     </React.Fragment>
   );
+}
+
+Post.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  forum: PropTypes.string.isRequired,
+  upvotes: PropTypes.number.isRequired,
+  downvotes: PropTypes.number.isRequired
 }
 
 export default Post;
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,11 +3,22 @@ import Post from "./Post";
 
 function PostList() {
   return (
-    <Post
-      author="fotograffer"
-      title="Sunset in Laguna Beach"
-      forum="nature"
-      date="2/2/2022"/>
+    <React.Fragment>
+      <Post
+        author="fotograffer"
+        title="Sunset in Laguna Beach"
+        forum="nature"
+        date="2/2/2022"
+        upvotes="145"
+        downvotes="24"/>
+      <Post
+        author="vinyln3rd"
+        title="Reissue of Around the Fur by Deftones"
+        forum="music"
+        date="4/25/2021"
+        upvotes="1787"
+        downvotes="0"/>
+    </React.Fragment>
   );
 }
 
