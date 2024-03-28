f23091599a01327638e6eba4748ce65c011abb71
Brian Scherner <brianscherner@gmail.com>
2024-03-28 13:44:10 -0700

    Add Redux to project, and write passing tests for combining both reducers into root reducer.

---
+++ src/__tests__/reducers/index-reducer.test.js
@@ -0,0 +1,48 @@
+import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import postListReducer from '../../reducers/post-list-reducer';
+import { act } from 'react-dom/test-utils';
+
+let store = createStore(rootReducer);
+
+describe('rootReducer', () => {
+
+  test('Should return default state if no action type is recognized.', () => {
+    expect(rootReducer({}, {type: null})).toEqual({
+      mainPostList: {},
+      formVisibleOnPage: false
+    });
+  });
+
+  test('Check that initial state of postListReducer matches root reducer.', () => {
+    expect(store.getState().mainPostList).toEqual(postListReducer(undefined, {type: null}));
+  });
+
+  test('Check that initial state of formVisibleReducer matches root reducer.', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, {type: null}));
+  });
+
+  test('Check that SUBMIT_POST action works for postListReducer and root reducer.', () => {
+    const action = {
+      type: 'SUBMIT_POST',
+      title: 'Holiday in Bali',
+      author: 'globetrotter',
+      date: '4/4/2023',
+      forum: 'pics',
+      upvotes: 0,
+      downvotes: 0,
+      id: 1
+    }
+    store.dispatch(action);
+    expect(store.getState().mainPostList).toEqual(postListReducer(undefined, action));
+  });
+
+  test('Check that TOGGLE_FORM action works for formVisibleReducer and root reducer.', () => {
+    const action = {
+      type: 'TOGGLE_FORM'
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
+  });
+});
